e946377158fe1a16e880a8a283f6a9c3017f1c66
zinkevish <zinkevish77@gmail.com>
2024-12-04 19:33:46 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,21 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []  # Создаем пустой список простых чисел
+not_primes = []  # Создаем пустой список не простых чисел
+
+def is_prime(n):  # Функция для проверки, является ли число простым
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for num in numbers:
+    if num != 1:
+        if is_prime(num):
+            primes.append(num)
+        else:
+            not_primes.append(num)
+
+print(primes)  # вывод на печать простые числа
+print(not_primes)  # вывод на печать не простые числа
